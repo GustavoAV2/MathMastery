210664a03fbcf90d865b0e7896065f74a4350ae0
gustavo.voltolini <gustavo.voltolini@ambevtech.com.br>
2023-01-17 16:15:43 -0300

    Fixing requisitions

---
+++ backend/HttpHost/FunctionalTests/Factories/HttpClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace FunctionalTests.Factories
+{
+    class HttpClientFactory
+    {
+        public static HttpClient CreateClient()
+        {
+            var factory = new WebApplicationFactory<HttpHost.Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.AddInMemoryCollection();
+                });
+            });
+
+            var httpClient = factory.CreateClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer TestToken");
+            return httpClient;
+        }
+    }
+}
