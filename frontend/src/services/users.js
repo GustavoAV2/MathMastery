19dd8fbdbbaaa6b2dffeac39d164632f3404957f
gustavo.voltolini <gustavo.voltolini@ambevtech.com.br>
2023-01-25 19:35:45 -0300

    Creating new request

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -7,17 +7,17 @@ export default{
     create(user){
         return http.post('/user', user)
     },
-    getById(id){
-        return http.get('/user/' + id)
+    getByToken(){
+        return http.get('/user/me')
     },
-    getByToken(token){
-        return http.get('/user/me/' + token)
-    },
-    getByUsername(username){
-        return http.get('/user/' + username)
+    getByUsernameOrEmail(username){
+        return http.get('/user/friend/' + username)
     },
     update(user, id){
         return http.put('/user/' + id, user)
+    },
+    updateGameUserStatus(payload, id){
+        return http.put('/user/game/' + id, payload)
     },
     desactive(id){
         return http.delete('/user/desactive/' + id)
