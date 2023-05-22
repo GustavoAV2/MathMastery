74d9317c85ded1c3acc6c55eda4a44ead6322849
gustavo.voltolini <gustavo.voltolini@ambevtech.com.br>
2023-05-21 23:42:41 -0300

    Fixing baseUrl

--- frontend/src/services/client.js
+++ frontend/src/services/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: import.meta.env.BACKEND_URL,
+    baseURL: "http://18.231.69.144:8000/",
     headers: {
         'Authorization': {
             toString() {
